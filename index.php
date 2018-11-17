db80bc07defd8d55d983f1a20c90e66d17cfcb71
Tom Venta <33782489+tomventa@users.noreply.github.com>
2018-11-18 00:44:18 +0100

    Modifiche alla pagina index, cambio estensione e test firma RSA (#12)
    * Modifiche al file index.php e test firma RSA
    
    * Test RSA

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
+<html>
 <head>
     <title>VoodooBuilder - Presto disponibile</title>
     <link rel="icon" type="image/png" href="/images/favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="/images/favicon-16x16.png" sizes="16x16" />
 </head>
-<body style="background-color: black">
-    <img src="/images/voodoobuilderLogo.png" alt="VoodooBuilder" class="center">
-</body>
-
 <style>
 .center {
     display: block;
@@ -15,3 +12,8 @@
     width: 50%;
 }
 </style>
+<body style="background-color: black">
+    <img src="/images/voodoobuilderLogo.png" alt="VoodooBuilder" class="center">
+</body>
+
+</html>
